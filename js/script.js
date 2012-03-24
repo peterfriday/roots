ec52d7ddbf56c299951ff37993e52cf564aa737c
Peter Friday <peterfriday@gmail.com>
2012-03-24 11:01:54 -0700

    changed formatting to 750px width for main and containers and adjusted styles to scale correctly for instagram feed

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(function(){
 			type: 'GET',
 			dataType: 'jsonp',
 			cache: false,
-			url: 'https://api.instagram.com/v1/users/self/media/recent?access_token=339836.3d6ab95.f70450982b6f4056855ee028a266da91&count=50',
+			url: 'https://api.instagram.com/v1/users/self/media/recent?access_token=339836.3d6ab95.f70450982b6f4056855ee028a266da91&count=40',
 			success: function(data){
 				var h = ''
 				for(i in data.data){
@@ -19,7 +19,7 @@ $(function(){
 					if(typeof item === "undefined") continue
 
 					url = item.images.standard_resolution.url
-					h += '<div class="span3"><img style="margin-bottom: 6px;" src="' + url + '" /></div>'
+					h += '<div class="span2"><img style="" src="' + url + '" /></div>'
 				}
 				console.log(h)
 				$('#main').html(h)
